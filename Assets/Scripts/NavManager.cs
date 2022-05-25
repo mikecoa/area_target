b65f483f8f876f504f356fc14c8537a58d2892e3
mikecoa <67162328+mikecoa@users.noreply.github.com>
2022-05-25 12:07:47 +0800

    s

--- Assets/Scripts/NavManager.cs
+++ Assets/Scripts/NavManager.cs
@@ -33,7 +33,6 @@ public class NavManager : MonoBehaviour
     public NavMeshPath path;
     public GameObject prefab;
     public Button findPathButton;
-    //float elapsed;
     public TextMeshProUGUI textMesh;
     private float distance;
     private bool reach = false;
@@ -42,17 +41,15 @@ public class NavManager : MonoBehaviour
     public List<Vector3> posPath;
     public LookAtConstraint currentLookAtConstraint;
     public LayerMask ballsLayerMask;
-    //public List<Vector3> turnPos;
     public GameObject turn;
     public List<GameObject> turns;
-    //public GameObject corner;
+
 
     // Start is called before the first frame update
     void Start()
     {
         path = new NavMeshPath();
         findPathButton.onClick.AddListener(FindPath);
-        //elapsed = 0.0f;
         nicCanvas.constraintActive = false;
         mikeCanvas.constraintActive = false;
         benCanvas.constraintActive = false;
@@ -61,36 +58,7 @@ public class NavManager : MonoBehaviour
     // Update is called once per frame
     public void Update()
     {
-
-        //List<Vector3> corners = new List<Vector3>();
         if (start.transform.position == end.transform.position) Reset();
-        
-        // Update the way to the goal every second.
-        /*elapsed += Time.deltaTime;
-        if (elapsed > 0.1f)
-        {
-            elapsed -= 0.1f;
-            if (NavMesh.SamplePosition(agent.transform.position, out hit, 5.0f, NavMesh.AllAreas))
-            {
-                NavMesh.CalculatePath(hit.position, end.position, NavMesh.AllAreas, path);
-            }
-        }*/
-        
-        /*if (NavMesh.SamplePosition(agent.transform.position, out hit, 5.0f, NavMesh.AllAreas))
-        {
-            NavMesh.CalculatePath(hit.position, end.position, NavMesh.AllAreas, path);
-        }
-        lineRenderer.positionCount = path.corners.Length;
-        // lineRenderer.positionCount = 2;
-        //
-        // foreach(Vector3 c in path.corners)
-        // {
-        //     corners.Add(c + Vector3.up * 1);
-        // }
-        // lineRenderer.SetPositions(corners.ToArray());
-        lineRenderer.SetPositions(path.corners);*/
-
-
         distance = Vector3.Distance (agent.transform.position, end.transform.position);
         if (distance < distance_adjust)
         {
@@ -117,26 +85,7 @@ public class NavManager : MonoBehaviour
         }
 
         if (reach && distance > distance_adjust) Reset();
-
-        /*for (int i = 0; i < posPath.Count; i++)
-        {
-            if (Vector3.Distance(agent.transform.position, posPath[i]) < 1)
-            {
-                spheres[i].SetActive(false);
-            }
-        }*/
-
-        /*foreach (Vector3 t in turnPos)
-        {
-            if (Vector3.Distance(t, agent.transform.position) < 0.5f)
-            {
-                turns.Add(Instantiate(turn, t + Vector3.back + Vector3.up * 0.5f, Quaternion.identity,canvas));
-            }
-            else
-            {
-                turns.Clear();
-            }
-        }*/
+        
         Collider[] objs;
         objs = Physics.OverlapSphere(agent.transform.position + Vector3.up, 1, ballsLayerMask);
         foreach (Collider c in objs)
@@ -146,107 +95,91 @@ public class NavManager : MonoBehaviour
     }
     List<GameObject> spheres = new List<GameObject>();
     private List<GameObject> corners = new List<GameObject>();
+
     public void FindPath()
     {
         List<Vector3> posCorners = new List<Vector3>();
         posPath = new List<Vector3>();
-        //turnPos = new List<Vector3>();
         Vector3 curPos, curUnitVector, vec;
         float dis, intervals;
-        DestroySpheres();  
+        DestroySpheres();
         if (NavMesh.SamplePosition(agent.transform.position, out hit, 5.0f, NavMesh.AllAreas))
         {
             NavMesh.CalculatePath(hit.position, end.position, NavMesh.AllAreas, path);
         }
-        /*lineRenderer.positionCount = path.corners.Length;
-        lineRenderer.SetPositions(path.corners);*/
-        
-        foreach(Vector3 c in path.corners)
+
+        foreach (Vector3 c in path.corners)
         {
-             posCorners.Add(c);
-             //corners.Add(Instantiate(corner, c+Vector3.up*1.25f,Quaternion.identity,canvas));
+            posCorners.Add(c);
         }
 
         intervals = 1;
         curPos = posCorners[0];
-        for (int i = 0; i < path.corners.Length-1 ; i++)
+        for (int i = 0; i < path.corners.Length - 1; i++)
         {
             if (Vector3.Distance(curPos, posCorners[i + 1]) >= intervals)
             {
-                posPath.Add(posCorners[i]+Vector3.up * 1.25f);
+                posPath.Add(posCorners[i] + Vector3.up * 1.25f);
             }
+
             curPos = posCorners[i];
-            vec = posCorners[i+1] - posCorners[i];
-            dis = Vector3.Distance(posCorners[i], posCorners[i+1]);
+            vec = posCorners[i + 1] - posCorners[i];
+            dis = Vector3.Distance(posCorners[i], posCorners[i + 1]);
             curUnitVector.x = vec.x / dis * intervals;
             curUnitVector.y = vec.y / dis * intervals;
             curUnitVector.z = vec.z / dis * intervals;
-            while (Vector3.Distance(curPos, posCorners[i+1]) >= intervals)
+            while (Vector3.Distance(curPos, posCorners[i + 1]) >= intervals)
             {
                 curPos = curPos + curUnitVector;
-                posPath.Add(curPos+Vector3.up * 1.25f);
+                posPath.Add(curPos + Vector3.up * 1.25f);
             }
         }
+
         foreach (Vector3 p in posPath)
         {
-            spheres.Add(Instantiate(prefab, p, Quaternion.identity,canvas));
+            spheres.Add(Instantiate(prefab, p, Quaternion.identity, canvas));
         }
 
         int j = 0;
         GameObject t;
         Vector3 eulerAngles;
-        //int dir; //-1 left, 1 right, 0 straight
-        //Vector3 nextUnitVector;
         foreach (Vector3 p in posCorners)
         {
-            /*dir = 0;
-
-            if (j >= 1 && j <= posCorners.Count-2)
+            if (!(j == 0 || j == posCorners.Count - 1))
             {
-                Debug.Log(FindTeta((posCorners[j] - posCorners[j - 1]) * -1, posCorners[j+1] - posCorners[j]));
-            }
-            else
-            {
-                Debug.Log("EMEM");
-            }*/
-            
-            
-            if (!(j == 0 || j == posCorners.Count-1))
-            {
-                vec = posCorners[j] - posCorners[j-1];
-                dis = Vector3.Distance(posCorners[j-1], posCorners[j]);
+                vec = posCorners[j] - posCorners[j - 1];
+                dis = Vector3.Distance(posCorners[j - 1], posCorners[j]);
                 curUnitVector.x = vec.x / dis * intervals;
                 curUnitVector.y = vec.y / dis * intervals;
                 curUnitVector.z = vec.z / dis * intervals;
-                if (Vector3.Distance(posCorners[j-1], posCorners[j]) > intervals)
+                if (Vector3.Distance(posCorners[j - 1], posCorners[j]) > intervals)
                 {
                     t = Instantiate(turn, p + Vector3.up * 1.25f, Quaternion.identity, canvas);
                     t.transform.position += curUnitVector * 0.8f;
                     turns.Add(t);
                 }
             }
+
             j++;
         }
 
-        for (int i = 0; i < turns.Count - 1; i++)
+        if (turns.Count < 1) return;
         {
-            turns[i].transform.LookAt(turns[i+1].transform);
-            eulerAngles = turns[i].transform.rotation.eulerAngles;
+            for (int i = 0; i < turns.Count - 1; i++)
+            {
+                turns[i].transform.LookAt(turns[i + 1].transform);
+                eulerAngles = turns[i].transform.rotation.eulerAngles;
+                eulerAngles = new Vector3(0, eulerAngles.y + 90, 0);
+                turns[i].transform.rotation = Quaternion.Euler(eulerAngles);
+            }
+
+            turns[turns.Count - 1].transform.LookAt(end.transform);
+            eulerAngles = turns[turns.Count - 1].transform.rotation.eulerAngles;
             eulerAngles = new Vector3(0, eulerAngles.y + 90, 0);
-            turns[i].transform.rotation = Quaternion.Euler(eulerAngles);
+            turns[turns.Count - 1].transform.rotation = Quaternion.Euler(eulerAngles);
         }
-        turns[turns.Count-1].transform.LookAt(end.transform);
-        eulerAngles = turns[turns.Count-1].transform.rotation.eulerAngles;
-        eulerAngles = new Vector3(0, eulerAngles.y + 90, 0);
-        turns[turns.Count-1].transform.rotation = Quaternion.Euler(eulerAngles);
     }
 
-    /*public float FindTeta(Vector3 ba, Vector3 bc)
-    {
-        return Mathf.Acos(Vector3.Dot(ba, bc) /
-                          ((Vector3.Distance(ba, Vector3.zero)) * Vector3.Distance(bc, Vector3.zero)));
-    }*/
-    
     public void Reset()
     {
         Nicolas.SetActive(false);
